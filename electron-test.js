0f8c86d2518597736e02bce5e99371b449d6359e
CC-Developer <cc-dev@example.com>
2026-05-25 18:44:13 +0800

    清理: 移除误提交的临时文件，.gitignore补充规则

--- electron-test.js
+++
@@ -1,5 +0,0 @@
-const electron = require('electron');
-console.log('typeof electron:', typeof electron);
-console.log('keys:', Object.keys(electron).slice(0, 10));
-console.log('app:', typeof electron.app);
-process.exit(0);
